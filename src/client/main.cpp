e0cd9523ff97911420547536cf1b2dc747f7fe89
Jean Bisel <jean@langmatt.com>
2019-12-30 20:55:41 +0100

    From swap chains to shaders

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,9 +1,30 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
 #include <vector>
 void err(int errnum, const char* err){
 	std::cerr << errnum << " -- " << err << std::endl;
+}
+VkShaderModule loadShaderModuleFromFile(VkDevice device,std::string filename){
+	std::ifstream file(filename,std::ios::ate | std::ios::binary);
+	unsigned int filesize = file.tellg();
+	file.seekg(0);
+	std::vector<char> bytecode(filesize);
+	file.read(bytecode.data(), filesize);
+	VkShaderModuleCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.codeSize = bytecode.size();
+	createInfo.pCode = reinterpret_cast<const uint32_t*>(bytecode.data());
+	VkShaderModule shaderModule;
+	if(vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule)==VK_SUCCESS){
+		std::cout << "Loaded shader " << filename << std::endl;
+		return shaderModule;
+	}
+	else{
+		std::cerr << "Failed to load shader " << filename << std::endl;
+		return NULL;
+	}
 }
 int main(){
 	glfwSetErrorCallback(err);
@@ -40,23 +61,36 @@ int main(){
 	createInfo.pApplicationInfo = &applicationInfo;
 
 	unsigned int count=0;
-	auto exts = glfwGetRequiredInstanceExtensions(&count);
-	createInfo.enabledExtensionCount = count;
-	createInfo.ppEnabledExtensionNames = exts;
-	std::cout << "Loaded extensions:" << std::endl;
+	const char** exts = glfwGetRequiredInstanceExtensions(&count);
+	std::vector<const char*> extensionVector;
 	for(unsigned int i=0;i<count;i++){
-		std::cout << "\t- " << createInfo.ppEnabledExtensionNames[i] << std::endl;
+		extensionVector.push_back(exts[i]);
+	}
+	extensionVector.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	createInfo.enabledExtensionCount = count;
+	createInfo.ppEnabledExtensionNames = extensionVector.data();
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+	std::vector<const char*> selectedLayers;
+	selectedLayers.push_back("VK_LAYER_KHRONOS_validation");
+	createInfo.enabledLayerCount = selectedLayers.size();
+	createInfo.ppEnabledLayerNames=selectedLayers.data();
+	std::cout << "Available Layers:" << std::endl;
+	for(unsigned int i=0;i<availableLayers.size();i++){
+		std::cout << "\t- " << availableLayers[i].layerName << std::endl;
+	}
+	std::cout << "Selected Layers:" << std::endl;
+	for(unsigned int i=0;i<selectedLayers.size();i++){
+		std::cout << "\t- " << selectedLayers[i] << std::endl;
+	}
+	std::cout << "Loaded extensions:" << std::endl;
+	for(unsigned int i=0;i<extensionVector.size();i++){
+		std::cout << "\t- " << extensionVector[i] << std::endl;
 	}
 	
 	vkCreateInstance(&createInfo, NULL,&vkinstance);
-//	if (vkCreateInstance(&createInfo, nullptr, &vkinstance) != VK_SUCCESS) {
-//		throw std::runtime_error("failed to create instance!");
-//	}
-//	uint32_t extensionCount = 0;
-//	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
-//	std::vector<VkExtensionProperties> extensions(extensionCount);
-//	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensions.data());
-//
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	uint32_t deviceCount = 0;
 	vkEnumeratePhysicalDevices(vkinstance, &deviceCount, nullptr);
@@ -101,6 +135,11 @@ int main(){
 	devCreateInfo.pQueueCreateInfos = &queueCreateInfo;
 	devCreateInfo.queueCreateInfoCount = 1;
 	devCreateInfo.pEnabledFeatures = &deviceFeatures;
+
+	std::vector<const char*> logicalExtensionVector;
+	logicalExtensionVector.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+	devCreateInfo.enabledExtensionCount = logicalExtensionVector.size();
+	devCreateInfo.ppEnabledExtensionNames = logicalExtensionVector.data();
 	VkDevice device = nullptr;
 	if (vkCreateDevice(physicalDevice, &devCreateInfo, nullptr, &device) != VK_SUCCESS) {
 		std::cerr << "Failed to create logical device." << std::endl ;
@@ -113,11 +152,131 @@ int main(){
 		std::cerr << "Cannot create window surface " << std::endl ;
 		glfwTerminate();
 	}
+	VkBool32 presentSupport = false;
+	vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, graphicsFamilyID, surface, &presentSupport);
+	if(presentSupport){
+		std::cout << "Queue supports presentation." << std::endl;
+	}
+	else{
+		std::cerr << "This program is not designed to support presentation on a different queue than graphics." << std::endl;
+		glfwTerminate();
+	}
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &capabilities);
+
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, nullptr);
+	if (formatCount != 0) {
+		formats.resize(formatCount);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, formats.data());
+	}
+
+	uint32_t presentModeCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, nullptr);
+	if (presentModeCount != 0) {
+		presentModes.resize(presentModeCount);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes.data());
+	}
 	
+	VkSurfaceFormatKHR format = formats[0];
+	VkPresentModeKHR presentMode = VK_PRESENT_MODE_FIFO_KHR;
+	for(unsigned int i=0;i<presentModeCount;i++){
+		if(presentModes[i]==VK_PRESENT_MODE_MAILBOX_KHR){
+			presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+			std::cout << "Surface supports triple buffering." << std::endl;
+		}
+	}
+	VkExtent2D extent = {800,600};
+	uint32_t imageCount=capabilities.minImageCount+1;
+	if(capabilities.minImageCount == capabilities.maxImageCount){
+		imageCount-=1;
+	}
+	VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
+	swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	swapchainCreateInfo.surface = surface;
+	swapchainCreateInfo.minImageCount = imageCount;
+	swapchainCreateInfo.imageFormat = format.format;
+	swapchainCreateInfo.imageColorSpace = format.colorSpace;
+	swapchainCreateInfo.imageExtent = extent;
+	swapchainCreateInfo.imageArrayLayers = 1;
+	swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	swapchainCreateInfo.preTransform = capabilities.currentTransform;
+	swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	swapchainCreateInfo.presentMode = presentMode;
+	swapchainCreateInfo.clipped = VK_TRUE;
+	swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+	VkSwapchainKHR swapchain;
+	if(vkCreateSwapchainKHR(device,&swapchainCreateInfo,nullptr,&swapchain) != VK_SUCCESS){
+		std::cerr << "Could not create swap chain." << std::endl;
+		glfwTerminate();
+	}
+	else{
+		std::cout << "Swapchain successfully created." << std::endl;
+	}
+	std::vector<VkImage> swapChainImages;
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr);
+	swapChainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, swapChainImages.data());
+	VkFormat swapChainImageFormat = format.format;
+	std::vector<VkImageView> ImageViews;
+	ImageViews.resize(imageCount);
+	for(unsigned int i=0;i<imageCount;i++){
+		VkImageViewCreateInfo imageViewCreateInfo = {};
+		imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		imageViewCreateInfo.image = swapChainImages[i];
+		imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		imageViewCreateInfo.format = swapChainImageFormat;
+		imageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+		imageViewCreateInfo.subresourceRange.levelCount = 1;
+		imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+		imageViewCreateInfo.subresourceRange.layerCount = 1;
+		if(vkCreateImageView(device,&imageViewCreateInfo,nullptr,&ImageViews[i])!=VK_SUCCESS){
+			std::cerr << "Could not create Image View." << i << std::endl;
+			glfwTerminate();
+		}
+
+	}
+
+	auto fragmentShaderModule = loadShaderModuleFromFile(device,"frag.sprv");
+	auto vertexShaderModule = loadShaderModuleFromFile(device,"vert.sprv");
+
+	VkPipelineShaderStageCreateInfo vertexShaderStageInfo = {};
+	vertexShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vertexShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+
+	vertexShaderStageInfo.module = vertexShaderModule;
+	vertexShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo fragmentShaderStageInfo = {};
+	fragmentShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fragmentShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fragmentShaderStageInfo.module = fragmentShaderModule;
+	fragmentShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo shaderStages[] = {vertexShaderStageInfo, fragmentShaderStageInfo};
+
+
 	while(!glfwWindowShouldClose(window)){
 		glfwPollEvents();
 
 	}
+
+
+	for(unsigned int i=0;i<imageCount;i++){
+		vkDestroyImageView(device,ImageViews[i],nullptr);
+	}
+
+	vkDestroyShaderModule(device, fragmentShaderModule, nullptr);
+	vkDestroyShaderModule(device, vertexShaderModule, nullptr);
+	vkDestroySwapchainKHR(device, swapchain, nullptr);
 	vkDestroyDevice(device, nullptr);
 	vkDestroySurfaceKHR(vkinstance,surface, nullptr);
 	vkDestroyInstance(vkinstance, NULL);
